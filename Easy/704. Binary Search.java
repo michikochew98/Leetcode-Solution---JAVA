a697b6900c24c4db4d7d5615f7e2365f93259588
michiko chew <michikochew98@gmail.com>
2022-11-15 12:13:28 +0800

    704. Binary Search(using upper/lower boundary concept)

--- Easy/704. Binary Search.java
+++ Easy/704. Binary Search.java
@@ -26,3 +26,41 @@ class Solution {
         return -1;
     }
 }
+
+  /*
+ * Binary search (using upper/lower boundary concept)
+ * 
+ * Time complexity: O(log N) where N represent the length of array
+ * 
+ * Space complexity : O(1)
+ * 
+ */
+
+class Solution {
+
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start < end) { // loop exits at (start == end) or (start < end)
+            int middle = start + (end - start + 1) / 2; 
+
+            if(nums[middle] > target) end = middle - 1; // exclude mid elem as nums[middle] is greater than
+            else start = middle;
+            
+            /*
+
+            or
+
+            int middle = start + (end - start) / 2; // make sure this condition satisfied arrow pointed statement and exit loop
+                                                    // when two element left within boundary
+                                                    // eg: start = 0, end = 1
+
+            if(nums[middle] < target) start = middle + 1; // exclude mid elem as nums[middle] is smaller than
+            else end = middle; // <- check here
+            */
+        }
+        
+        return nums[start] == target ? start : -1;
+    }
+}
